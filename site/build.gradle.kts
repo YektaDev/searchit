59af58cc9a33fafc9dc6e49dc76c7ee142b27a62
Ali Khaleqi Yekta <me@yekta.dev>
2023-12-24 08:27:35 +0330

    conf(dependencies): Add Kotlin Serialization

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -2,6 +2,7 @@ import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kobweb.application)
     alias(libs.plugins.kobwebx.markdown)
@@ -26,6 +27,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(libs.kotlinx.serialization.json)
                 implementation(compose.runtime)
             }
         }
